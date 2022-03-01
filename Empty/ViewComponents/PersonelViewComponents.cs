311df8712ef8400f2c4618181953c59cbd34d6ca
hakan çakır <baranc3363@gmail.com>
2022-03-01 16:38:53 +0300

    vc

---
+++ Empty/ViewComponents/PersonelViewComponents.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Empty.ViewComponents
+{
+    public class PersonelViewComponent : ViewComponent
+    {
+       public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
