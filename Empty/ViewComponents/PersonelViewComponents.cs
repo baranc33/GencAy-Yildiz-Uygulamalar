c6058c7b0f275e50a9476b3db9c43189c20be807
hakan çakır <baranc3363@gmail.com>
2022-03-02 09:40:08 +0300

    LastPartialView

--- Empty/ViewComponents/PersonelViewComponents.cs
+++ Empty/ViewComponents/PersonelViewComponents.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Empty.ViewComponents
 {
+    //[NonViewComponent] Bu şekilde render edilmesini engelleyebiliriz
     public class PersonelViewComponent : ViewComponent
     {
-       public IViewComponentResult Invoke()
+        // aldığı parametre açğrıldı yerde geçilir parametre almak zorunda değil
+        public IViewComponentResult Invoke(int id)
         {
+            // bu verilerin veri tabanından geldiğini farz ediyorum
             List<Personel> datas = new List<Personel>()
             {
                 new Personel{Adi="Hakan Baran",SoyAdi="Çakır"},
