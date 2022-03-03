a5805df884b6bd6d0e14df20ad5c11ce2b99209f
hakan çakır <baranc3363@gmail.com>
2022-03-03 19:59:16 +0300

    endpoints.map

--- CustomRootHandler/Program.cs
+++ CustomRootHandler/Program.cs
@@ -20,6 +20,18 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseEndpoints(endpoints =>
+{
+    // 1. parametrede oluþturulan controller dýþý gidecek EndPoint yani istek þekli.
+    // 2. parametrede istersek bir sýnýfa yönlendiririz istersek direk burda fonksyonnu yazabiliriz
+    // bu örnekte fonksyonu uygun bir Task<delegate> alýr biz bunu lamda yardýmýyla bu delageti yazcaz
+    endpoints.Map("example-route",async x=>
+    {
+        // burda yapýlcak iþlemleriz
+        //https://localhost:5001/example-route controller gitmez burda iþlem yapar
+    });
+});
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
