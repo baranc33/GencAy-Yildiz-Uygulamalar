7abf8d9f44d69aa9fd28d9383a9f9dd56d057c06
hakan çakır <baranc3363@gmail.com>
2022-03-03 19:50:25 +0300

    CustomRootHandler

---
+++ CustomRootHandler/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomRootHandler.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
