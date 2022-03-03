ae506857bf3c805e582adf0f7dcd3f2d22073e4a
hakan çakır <baranc3363@gmail.com>
2022-03-03 05:21:31 +0300

    Route

---
+++ RouteYapilanmasi/Constraints/CustomConstraint.cs
@@ -0,0 +1,17 @@
+﻿namespace RouteYapilanmasi.Constraints
+{
+    public class CustomConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContext? httpContext, 
+                        IRouter? route, 
+                        string routeKey,
+                        RouteValueDictionary values, 
+                        RouteDirection routeDirection)
+        {
+            var idvalue=values[routeKey]; 
+            // burda belirttiğimiz kontrolleri yapabilir
+            // 5 ten büyükmü içinde harf varmı bir nesne ise bu nesne değerleri dolumu vs.
+            return true;
+        }
+    }
+}
---
+++ RouteYapilanmasi/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RouteYapilanmasi.Models;
+using System.Diagnostics;
+
+namespace RouteYapilanmasi.Controllers
+{
+
+    [Route("AnaSayfa")]
+        public class HomeController : Controller
+        {
+            private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("IlkSayfa/{id?}")]
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
