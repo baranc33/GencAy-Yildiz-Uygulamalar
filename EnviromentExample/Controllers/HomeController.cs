64f0f280e0bd7acf06e53a574f72ff7543832862
hakan çakır <baranc3363@gmail.com>
2022-03-06 16:21:50 +0300

    Enviroment

---
+++ EnviromentExample/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using EnviromentExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EnviromentExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            _environment = environment;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+
+            var a = _configuration["Deneme"];
+            var b = _configuration["Hakan"];
+            bool Enviroment = _environment.IsEnvironment("MyEnviroment");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sercretManagerExample/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace sercretManagerExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        readonly IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+
+        public  void Index()
+        {
+            var connection = _configuration.GetConnectionString("SQL");
+
+            SqlConnectionStringBuilder builder= new SqlConnectionStringBuilder();
+            builder.UserID = _configuration["SQL:KullaniciAdi"];
+            builder.Password= _configuration["Sifre"];
+            var ConnectionString = builder.ConnectionString;
+        }
+
+    }
+}
