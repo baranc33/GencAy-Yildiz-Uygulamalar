0d59fd119d4d7a8d452e86f72b5b98e4c44bcaac
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-23 01:00:01 +0300

    Soru 1

---
+++ Algoritmalar/Algoritmalar/Program.cs
@@ -0,0 +1,19 @@
+﻿// iki sayının toplamını veren uygulamayı yazınız
+// dizileri tek satırda tanımlayıp kod maliyetini azaltırız
+decimal sayi1, sayi2;
+// kullanıcıya gerekli bilgiyi vermeliyiz
+try
+{// hata alabileceğimiz kısmı kontrol etmeliyiz
+
+    Console.WriteLine("Lütfen bir sayı giriniz");
+    sayi1 = Convert.ToDecimal(Console.ReadLine());
+
+    Console.WriteLine("Lütfen bir sayı giriniz");
+    sayi2 = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine(sayi1 + sayi2);
+}
+catch 
+{
+    Console.WriteLine("Lütfen Geçerli bir değer giriniz");
+
+}
