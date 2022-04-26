d61f90b2d04f3d97b10d4097f5040e26ce76373c
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-27 00:00:44 +0300

    soru 7 benim çözüm

---
+++ Algoritmalar/Soru7/Program.cs
@@ -0,0 +1,20 @@
+﻿// pozitif sayılarda bölme işlemini çıkarma kullanarak yapan uygulama yazınız.
+
+/*Çözüm  1*/
+Console.WriteLine("Lütfen Bölmek istediğiniz Sayıyı giriniz");
+int sayi1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Lütfen Bölen Sayıyı giriniz");
+int sayi2 = int.Parse(Console.ReadLine());
+int sonuc = 0;
+
+for (int i = 0; sayi2 <= sayi1; i++)// i bölünenden küçük olması gerekiyor bölme işlemini yapabilmek için
+{
+    sayi1-=sayi2;
+    sonuc++;
+}
+
+Console.WriteLine(sonuc);
+/*Çözüm  2*/
+/*Çözüm  3*/
+/*Çözüm  4*/
+/*Çözüm  5*/
