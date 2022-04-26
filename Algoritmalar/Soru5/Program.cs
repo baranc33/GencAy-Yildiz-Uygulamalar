d4ad79f3de091f5db712095740aebb5c6027062d
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-26 19:23:37 +0300

    soru 5 çözüm 3

--- Algoritmalar/Soru5/Program.cs
+++ Algoritmalar/Soru5/Program.cs
@@ -15,7 +15,7 @@ for (int i = sayi; i>0; i--)
 
 Console.WriteLine(sonuc);
 */
-/* Çözüm 2*/
+/* Çözüm 2
 
 Console.WriteLine("Lütfen Bir Sayı Giriniz");
 int sayi = int.Parse(Console.ReadLine());
@@ -25,10 +25,23 @@ while (sayi >0)
 {
     sonuc*=sayi;
     sayi--;
-
 }
 Console.WriteLine(sonuc);
-
+*/
 
 /* Çözüm 3*/
+
+Console.WriteLine("Lütfen Bir Sayı Giriniz");
+int sayi = int.Parse(Console.ReadLine());
+int sonuc = 1;
+
+
+do
+{
+    sonuc*=sayi;
+    sayi--;
+}
+while (sayi>0);
+Console.WriteLine(sonuc);
+
 /* Çözüm 4*/
