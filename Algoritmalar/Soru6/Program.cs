ba35718d00e3fe2a1b3c521cc75dfc2e5f47a03d
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-26 23:27:42 +0300

    soru 6 çözüm 2

--- Algoritmalar/Soru6/Program.cs
+++ Algoritmalar/Soru6/Program.cs
@@ -16,6 +16,20 @@ for (int i = 0; i < sayi2; i++)
 Console.WriteLine($"{sayi1} x {sayi2} = {toplam}");*/
 /* Çözüm 2*/
 
+Console.WriteLine("Lütfen bir sayı giriniz");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Lütfen bir sayı giriniz");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+int sayac = sayi2;
+int sonuc = 0;
+while (sayac>0)
+{
+    sonuc+=sayi1;
+    sayac--;
+}
+Console.WriteLine($"{sayi1} x {sayi2} = {sonuc}"); 
+
 /* Çözüm 3*/
 /* Çözüm 4*/
 /* Çözüm 5 */
