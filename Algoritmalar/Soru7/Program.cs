2070accfeb445fa92eaf47a0fe3417a17eb92138
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-27 00:04:15 +0300

    soru7 kalan

--- Algoritmalar/Soru7/Program.cs
+++ Algoritmalar/Soru7/Program.cs
@@ -21,18 +21,23 @@ Console.WriteLine("Lütfen Bölmek istediğiniz Sayıyı giriniz");
 int sayi1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Lütfen Bölen Sayıyı giriniz");
 int sayi2 = int.Parse(Console.ReadLine());
-int _sayi1= sayi1;
+int _sayi1 = sayi1;
 int sonuc = 0;
+int kalan = 0;
 
 for (int i = 0; i < _sayi1; i++)// i bölünenden küçük olması gerekiyor bölme işlemini yapabilmek için
 {
     sayi1-=sayi2;
     sonuc++;
 
-    if (sayi1<sayi2) break;
+    if (sayi1<sayi2)
+    {
+        kalan=sayi1;
+        break;
+    }
 }
-
 Console.WriteLine($"{_sayi1} / {sayi2} = {sonuc}");
+Console.WriteLine($"Kalan = {kalan}");
 /*Çözüm  2*/
 /*Çözüm  3*/
 /*Çözüm  4*/
