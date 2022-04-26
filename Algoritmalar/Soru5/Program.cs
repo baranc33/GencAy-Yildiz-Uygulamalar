00e4967003b728a5832f4f4cb8b666efc691bf71
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-26 19:17:38 +0300

    soru 5 çözüm 1

--- Algoritmalar/Soru5/Program.cs
+++ Algoritmalar/Soru5/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// girilen sayının faktöriyelini hesaplayan uygulama yazınız
+
+
+/* Çözüm 1 */
+
+
+Console.WriteLine("Lütfen Bir Sayı Giriniz");
+int sayi = int.Parse(Console.ReadLine());
+int sonuc = 1;
+
+for (int i = sayi; i>0; i--)
+{
+    sonuc *=i;
+}
+
+Console.WriteLine(sonuc);
+
+/* Çözüm */
+/* Çözüm */
+/* Çözüm */
