a2d9eb75b86625e5f99fad6f24388b47c783434f
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-26 22:58:15 +0300

    soru 6 çözüm 1

--- Algoritmalar/Soru6/Program.cs
+++ Algoritmalar/Soru6/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  pozitif sayılarda çarpma işlemini toplama kulanarak bulan uygulamayı yazınız.
+
+/* Çözüm 1 */
+Console.WriteLine("Lütfen bir sayı giriniz");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Lütfen bir sayı giriniz");
+int sayi2  = Convert.ToInt32(Console.ReadLine());
+
+int toplam = 0;
+for (int i = 0; i < sayi2; i++)
+{
+    toplam+=sayi1;
+}
+
+
+Console.WriteLine(toplam);
