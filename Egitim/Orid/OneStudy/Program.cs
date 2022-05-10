6f82281e268873bd80fb476f1c7117251121d683
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-10 23:39:05 +0300

    first code

--- Egitim/Orid/OneStudy/Program.cs
+++ Egitim/Orid/OneStudy/Program.cs
@@ -6,13 +6,19 @@
 // Ram Bölümleri
 
 
+int sayi1 = 15;
+int sayi2 = 10;
 
+int sayi3 = sayi1 + sayi2;
+
+Console.WriteLine(sayi3);
+
+/*
 1.Stack bölgesi
 Değer(value) Tip dediğimiz nesneler burda kalır
 stack pointer dediğimiz bölgeler sayesinde doğrudan erişilebilir
 stack bölgesinde olan değişkenler  çalışma zamanı öncesi ramde ne kadar yer kapliyacağı bilinmesi gerekir
 bazı referans tipli değişkenlerin yoluda bu bölgede bulunur
-
 
 
 2.Heap bölgesi
@@ -25,28 +31,9 @@ stack bölgesine göre daha yavaş çalışır
 
 
 
-3.Register bölgesi
-stack ve heap a göre hızlıdır sınırlı sayıdaki yapı olduğu için çok sık kullanılan yapıları bu bölgede tutar
-bu bölgeye doğrudan erişimimiz yoktur bu erişimi jit derleyici otomatik olarak yapar
-
-
-
-4.Static bölge
+3.Static bölge
 Bellekte herhangi bir bölgeyi temsil eder 
 bu bölgedeki nesneler programın akışı boyunca kullanılacağı için hep tutulurlar 
 bir static nesne oluşturmak için  
 Static kelimesi kullanırız
-
-
-
-5.sabit bölge
-sabit(constant) bu bölgede programın kodları tutulur sadece okumak amaçlı oldukları için hızlılık açısından bazı durumlarda 
-ROM (read only memory )' de tutulurlar 
-
-
-
-
-6.ram olmayan bölge
-program kapatıldığında verilerin kaybolmasını istemiyorsak buraya yazılır hard disk gibi.
-
-
+*/
