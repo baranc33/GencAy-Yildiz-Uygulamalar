b6b8d6ca09cf4935b30f1f8bf3349262396152e2
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-11 16:26:13 +0300

    mantıksal oparatörler

--- Egitim/Orid/Operator/Program.cs
+++ Egitim/Orid/Operator/Program.cs
@@ -5,16 +5,36 @@
     Bölme   /
     Kalan   %
  */
-int sayi1, sayi2, sayi3;
-sayi1 = 10;
-sayi2 = 3;
-sayi3=sayi1 % sayi2;
 
+// sayi 1 =10 sayi2=5 sayi3=7  sayi4=2 sayi5=2  sayi6=3
+// 1.soru  sayi1 ve sayi2 nin toplamından sayi 3 ü çıkar  
+// 2.soru sayi1 ve sayi2 nin toplamı sayi6 ya böle
+// 3.soru  sayi1 ve sayi2 toplamını sayi4 ile çarp
+// 4.soru  sayi1 ve sayi2 toplamından sayı 3ü çıkar sonra sayi 4 le sayı 5i çarp önceki sonuçla çarp
+//int sayi1, sayi2, sayi3,sayi4,sayi5,sayi6; // 8 4
+//sayi1 = 10;
+//sayi2 = 3;
+//sayi3=sayi1 % sayi2;
 
-Console.WriteLine(sayi3);
+//Console.WriteLine(sayi3);
+/* Bölüm 2  Mantıksal işlemler
+    Küçüktür  <
+    Büyüktür  >
+    Küçük Eşit   <=
+    Büyük Eşit   >=
+    Eşittir      ==
+    Eşit Değildir.  !=
+ */
+int sayi1 = 3, sayi2 = 4, sayi3 = 7, sayi4 = 2, sayi5 = 2, sayi6 = 11;
 
+bool Sonuc = sayi1>sayi2;// false
+bool Sonuc2 = sayi1+sayi2 == sayi3; // true
+bool Sonuc3 = sayi1+sayi2 != sayi3; // false
+bool Sonuc4 = sayi1+sayi2 > sayi4+sayi5; // true
+bool Sonuc5 = sayi1+sayi2 > sayi3;// false
+bool Sonuc6 = sayi1+sayi2 >= sayi3;// false
 
-
+Console.WriteLine(Sonuc6);
 
 
 /*
