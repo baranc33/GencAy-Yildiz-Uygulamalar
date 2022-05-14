7ad4a4392c30a03253f59926b84736b689f2d4a3
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-14 15:43:09 +0300

    if else

---
+++ Egitim/Orid/ifElse/Program.cs
@@ -0,0 +1,37 @@
+﻿// Console.ReadLine() Metodu ile Konsoldan Yazılan Yazıyı Alabiliriz 
+// Gelen Veri String Tipindedir
+string Girdi = Console.ReadLine();
+
+int Sayi = Convert.ToInt32(Girdi);
+
+// girilen Sayının 25 tek küçük olup olmadığını Kontrol etcem
+
+if (Sayi >25)
+{
+    Console.WriteLine("Girilen Sayı 25 den büyüktür");
+}
+else
+{
+    Console.WriteLine("Girilen Sayı 25 den Küçüktür");
+}
+
+
+//if ( sayi1 > 20)
+//{
+//    Console.WriteLine("Girilen Sayı 20 den büyüktür");
+//}
+//else
+//{
+//    Console.WriteLine("Girilen Sayı 20 den Küçüktür");
+
+//}
+
+
+/* Karar Mekanizması
+  
+ if ( Şartımızı)
+{
+    // yapılacak işlemi
+}
+
+ */
