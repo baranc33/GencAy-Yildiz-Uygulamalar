57f9190cf38525942943b14eb95c84507fca05a2
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-14 14:38:43 +0300

    türk dönüşümü

---
+++ Egitim/Orid/TurDonusumleri/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Lütfen Bir Sayı Giriniz");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Lütfen İkinci Sayıyı Giriniz");
+int sayi2 = int.Parse(Console.ReadLine());
+Console.WriteLine(sayi1+sayi2);
+
+
+
+
+
+
+
+/*
+ 4 yöntem ile tür dönüşümü Sağliyabiliriz
+
+1. Convert Yöntemi 
+2. Parshe  
+
+3. Cast
+4. as 
+
+*/
