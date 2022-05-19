2db130279581550c70c29acd46e1ca4b0b58b342
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-20 01:22:13 +0300

    döngüler

--- Egitim/Orid/Donguler/Program.cs
+++ Egitim/Orid/Donguler/Program.cs
@@ -7,9 +7,11 @@
 4- Foreach => döngü değil itere etmektir fakat döngü gibi davranır
 5- Go to x => döngü değildir.
 
-ek kullanılabilirk = break ,continue;
-
+break => döngüyü bitirir
+continue => o anki döngüyü atlar;
  */
+
+
 
 /* 1 Goto x
  amacı belli bi yerden belli bi yere kodu geriye Sarmaya yarar
@@ -18,130 +20,47 @@ başlangıcı ve bitişi vardır
 
 x:       => başlangıç noktası 
 goto x;   => gidicek noktası
+
+while
+en basit döngüdür.
+sadece if gibi yazılır if gibi hareket eder
+if yerine while yazarız.
+ 
  */
-// kullanıcada 2 sayı gerekiyor
-// hangi işlem 
-// sonuc
-int sayi1, sayi2;
-string islem;// * => çarpma   / => bölme   + => toplama   - => çıkarma
-int sonuc = 0;
-x:
-Console.WriteLine("Lütfen 1. sayıyı giriniz");
-string girdi1 = Console.ReadLine();
-bool result1 = int.TryParse(girdi1, out sayi1);
+int i = 1;
+// 12345-12345-12345
+// 3 kere yazsın
+int counter = 0;
 
-if (result1==false)
+
+while (i < 4)
 {
-    Console.Clear();// consolu temizliyor
-    Console.WriteLine("Hatalı Bir Giriş Yaptınız Lütfen Tekrar Deneyiniz !!!");
-    Thread.Sleep(3000);// mili saniye cinsinden kodu beklet 
-    Console.Clear();
-    goto x;
+    if (i==2)
+    {
+        i++;// 3 3 3 
+        continue;
+    }
+    Console.WriteLine(i);// 1 3 1 3 1 3
+    i++;//  2 4 2 4 2 4
+
+    if (i==4 && counter < 2)
+    {
+
+        counter++;// 1 2
+        i=1;
+    }
+
 }
 
-Console.Clear();
-Console.WriteLine("1. Sayının Değeri = "+ sayi1);
-
-y:
-Console.WriteLine("Lütfen 2. sayıyı giriniz");
-string girdi2 = Console.ReadLine();
-Console.Clear();
-Console.WriteLine("1. Sayının Değeri = "+ sayi1+ "\n2. sayının değeri = " + girdi2);
-bool result2 = int.TryParse(girdi2, out sayi2);
-
-if (result2==false)
-{
-    Console.Clear();
-    Console.WriteLine("Hatalı giriş yaptınız Lütfen Tekrar Deneyiniz!!!");
-    Thread.Sleep(3000);// mili saniye cinsinden kodu beklet 
-    Console.Clear();
-    goto y;
-}
-z:
-Console.WriteLine("Toplama için ( + ) tıklayınız.");
-Console.WriteLine("Çıkarma için ( - ) tıklayınız.");
-Console.WriteLine("Çarpma için ( * ) tıklayınız.");
-Console.WriteLine("Bölme için ( / ) tıklayınız.");
-Console.WriteLine("Kalan için ( % ) tıklayınız.");
-
-islem = Console.ReadLine();
-
-bool Dogrumu = Alayli.islemDogrumu(islem);
-
-if (Dogrumu==false)
-{
-    Console.Clear();
-    Console.WriteLine("İşlemini doğru seçiniz bay yavşak!");
-    Thread.Sleep(1500);
-    goto z;
-}
-
-string cümle = Alayli.Hesaplama(sayi1, sayi2, islem);
-
-Console.WriteLine(cümle);
 
 
+// yunus
+// şafak  1 2 1 2 
+// ibo
+// memoş
+// hako
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//if (islem=="+")
-//{
-//    sonuc =sayi1+sayi2;
-//}
-//else if (islem=="-")
-//{
-//    sonuc =sayi1-sayi2;
-//}
-//else if (islem=="/")
-//{
-//    sonuc =sayi1/sayi2;
-//}
-//else if (islem=="*")
-//{
-//    sonuc =sayi1*sayi2;
-//}
-//else
-//{
-//    Console.Clear();
-//    Console.WriteLine("İşlemini doğru seçiniz bay yavşak!");
-//    Thread.Sleep(1500);
-//    goto z;
-//}
-//Console.Clear();
-//Console.WriteLine(sayi1 +"  "+islem+" "+sayi2 +" = "+sonuc.ToString());
-
-
-// toplu yorum ctrl+k , ctrl+ c
-// kaldırması  ctrl+k  , ctrl + u
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// false bir metot=> yunus
-
+Console.WriteLine("işlem bitti");
 
 
 
