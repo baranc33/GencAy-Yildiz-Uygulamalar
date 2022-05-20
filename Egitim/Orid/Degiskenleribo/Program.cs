07a221a20f11b7959f393c57f946e2cd88021b39
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-20 03:00:00 +0300

    değişkenler

---
+++ Egitim/Orid/Degiskenleribo/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+ Değişken lerle iligili bilgiler
+ 1- değişkne yazılımın en temel parçasıdır. ramde belli bir alan açılarak bilgileri kaydetmemize yarar
+ 2- değişkenin türü , adı ve değişkenin değeri vardır;
+
+
+ 
+ */
+
+
